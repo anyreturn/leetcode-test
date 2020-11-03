665fd34c29f99130dc9729f346b020c607289cdf
yequan.fyq <yequan.fyq@alibaba-inc.com>
2020-11-03 16:57:25 +0800

    commit 二叉树最近公共节点

--- src/main/java/leetcode/editor/cn/CommonFather.java
+++ src/main/java/leetcode/editor/cn/CommonFather.java
@@ -1,5 +1,7 @@
 package leetcode.editor.cn;
 
+import java.util.LinkedList;
+
 /**
  * https://blog.csdn.net/qq_15457239/article/details/51029858
  * https://blog.csdn.net/wangls56/article/details/88783783?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param
@@ -19,6 +21,7 @@ public class CommonFather {
     /**
      * 二叉树最近公共节点
      * https://blog.csdn.net/wangls56/article/details/88783783?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param
+     * 递归实现
      * @param root
      * @param a
      * @param b
@@ -34,19 +37,89 @@ public class CommonFather {
         if(root==b){
             return b;
         }
+        //先在左子树中查找节点，如果找到则返回节点 a或者 b,否则返回 null
         TreeNode left = commonFather(root.left,a,b);
         TreeNode right = commonFather(root.right,a,b);
 
+        //如果左右子树均不为空，表示已在当前节点的子节点中找到a,b节点，当前root节点为最近公共父节点
         if(left!=null && right!=null){
             return root;
         }
+        //如果 left不为空表示，在当前 root子节点中存在 left(a或者b、a和b),返回 left,表示其中一个节点已找到或者两个节点已找到
         if(left!=null){
             return left;
         }
+        //如果 right不为空表示，在当前 root子节点中存在 right(a或者b、a和b),返回 right,表示其中一个节点已找到或者两个节点已找到
         if(right!=null){
             return right;
         }
         return null;
+    }
+
+    /**
+     * 二叉树两节点的最近公共节点
+     * 非递归实现
+     * @param root
+     * @param a
+     * @param b
+     * @return
+     */
+    TreeNode findAncestor(TreeNode root, TreeNode a, TreeNode b){
+        if(root==null) {
+            return null;
+        }
+        //a节点路径
+        LinkedList<TreeNode> aPath = new LinkedList<>();
+        aPath.add(root);
+        this.getPath(root,a,aPath);
+        //b节点路径
+        LinkedList<TreeNode> bPath = new LinkedList<>();
+        bPath.add(root);
+        this.getPath(root,b,bPath);
+
+        TreeNode father = null;
+        for(int i=0; i<aPath.size() && i<bPath.size();i++){
+            if(aPath.get(i)==bPath.get(i)){
+                father = aPath.get(i);
+            }else {
+                break;
+            }
+        }
+        return father;
+    }
+
+    /**
+     * 查找节点的路径
+     * @param root
+     * @param node
+     * @param path
+     * @return
+     */
+    boolean getPath(TreeNode root, TreeNode node, LinkedList<TreeNode> path){
+        if(root==node){
+            return true;
+        }
+        //如果左子树不为空，左子树中查找
+        if(root.left!=null){
+            path.add(root.left);
+            if(getPath(root.left, node,path)){
+                return true;
+            }else {
+                path.remove(root.left);
+            }
+        }
+
+        //如果右子树不为空，在右子树中查找
+        if(root.right!=null){
+            path.add(root.right);
+            if(getPath(root.right, node,path)){
+                return true;
+            }else {
+                path.remove(root.right);
+            }
+        }
+        return false;
+
     }
 
     private class TreeNode{
